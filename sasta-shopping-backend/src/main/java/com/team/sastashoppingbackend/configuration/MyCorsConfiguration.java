083a9099af8f67daf93227ef515cf14371c52e04
VAMSI KRISHNA PEDDAPUDI <vamsi3353@gmail.com>
2023-02-16 13:57:11 +0530

    Configuration Update

---
+++ sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/configuration/MyCorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.team.sastashoppingbackend.configuration;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class MyCorsConfiguration {
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://example.com"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+    configuration.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source.getCorsConfigurations();
+  }
+}
