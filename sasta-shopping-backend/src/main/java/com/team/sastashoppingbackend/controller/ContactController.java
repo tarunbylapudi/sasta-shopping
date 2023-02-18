8610468a9a3b383ac31e2fffa3def82ee3f95d14
VAMSI KRISHNA PEDDAPUDI <vamsi3353@gmail.com>
2023-02-18 23:49:57 +0530

    corss Change

--- sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/controller/ContactController.java
+++ sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/controller/ContactController.java
@@ -2,17 +2,18 @@ package com.team.sastashoppingbackend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team.sastashoppingbackend.entity.Contact;
-import com.team.sastashoppingbackend.entity.Customer;
 import com.team.sastashoppingbackend.service.ContactService;
 
 @RestController
 @RequestMapping("/contactQueries")
+@CrossOrigin(origins = "*")
 public class ContactController {
 	private ContactService contactService;
 	
