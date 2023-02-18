6246d1e45e996336314050eefe934e4d6fba7b7a
sai8charan <65341947+sai8charan@users.noreply.github.com>
2023-02-19 00:52:49 +0530

    Update ExceptionPayload.java

--- sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/exception/ExceptionPayload.java
+++ sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/exception/ExceptionPayload.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ExceptionPayload extends Exception {
+public class ExceptionPayload {
 	private String error;
 	private int code;
 	private Date timeStamp;
