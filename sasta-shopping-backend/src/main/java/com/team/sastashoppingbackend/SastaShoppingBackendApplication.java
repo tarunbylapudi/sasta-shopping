f0c530b2091483e4e1eaf55a9db0131db242ed0f
sai8charan <65341947+sai8charan@users.noreply.github.com>
2023-02-21 00:27:46 +0530

    cors

--- sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/SastaShoppingBackendApplication.java
+++ sasta-shopping-backend/src/main/java/com/team/sastashoppingbackend/SastaShoppingBackendApplication.java
@@ -2,12 +2,27 @@ package com.team.sastashoppingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SastaShoppingBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SastaShoppingBackendApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
 	}
 
 }
